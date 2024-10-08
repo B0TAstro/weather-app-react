894d8690590ca5d66c54e43819563e318d26425c
B0TAstro <148534949+B0TAstro@users.noreply.github.com>
2024-10-08 17:00:33 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,67 @@
 import React, { useState } from "react";
-import axios from "axios"; // Import axios
+import axios from "axios";
 
 function App() {
-  //const api_url = 'https://api.openweathermap.org/data/2.5/weather?q=chambéry&appid={c8767ecf3fe8ac92944b5668e9ea1173&lang=fr&units=metric}';
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const api_key = process.env.REACT_APP_WEATHER_API_KEY;
+  const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}&lang=fr&units=metric`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(api_url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Erreur lors de la récupération des données météo :", error.response ? error.response.data : error.message);
+        });
+      setLocation('');
+    }
+  }  
 
   return (
-    <div className="App">
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Rechercher une ville"
+          type="text"
+        />
+      </div>
       <div className="container">
-        <h1>Weather App</h1>
         <div className="top">
           <div className="location">
-            <p>Chambéry</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <p>20°C</p>
+            {data.main ? <h1>{Math.round(data.main.temp)}°C</h1> : null}
           </div>
           <div className="description">
-            <p>Ensoleillé</p>
+            {data.weather ? <p>{data.weather[0].description}</p> : null}
           </div>
         </div>
+
+        {data.name !== undefined &&
         <div className="bottom">
           <div className="feels">
-            <p>25°C</p>
+            {data.main ? <p className="bold">{Math.round(data.main.feels_like)}°C</p> : null}
+            <p>Ressentie</p>
           </div>
           <div className="humidity">
-            <p>35%</p>
+            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+            <p>Humidité</p>
           </div>
           <div className="wind">
-            <p>10km/h</p>
+            {data.wind ? <p className="bold">{Math.round(data.wind.speed)}KM/H</p> : null}
+            <p>Vent</p>
           </div>
         </div>
+        }
       </div>
     </div>
   );
