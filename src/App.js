2ff0801fdd688a70365915aab31b224b4874e92b
B0TAstro <148534949+B0TAstro@users.noreply.github.com>
2024-10-09 08:26:56 +0200

    V1.5 (ajout du fond d'écran changant)

--- src/App.js
+++ src/App.js
@@ -3,27 +3,51 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState("");
 
   const api_key = process.env.REACT_APP_WEATHER_API_KEY;
   const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}&lang=fr&units=metric`;
 
+  const getWeatherClass = () => {
+    if (!data.weather) return "";
+
+    const weatherId = data.weather[0].id;
+    if (weatherId === 800) {
+      return "sun";
+    }
+    if (weatherId >= 801 && weatherId <= 802) {
+      return "few_clouds";
+    }
+    if (weatherId >= 803 && weatherId <= 804) {
+      return "clouds";
+    }
+    if (weatherId >= 500 && weatherId <= 531) {
+      return "rain";
+    }
+    if (weatherId >= 600 && weatherId <= 622) {
+      return "snow";
+    }
+    if (weatherId >= 600 && weatherId <= 622) {
+      return "mist";
+    }
+    if (weatherId >= 200 && weatherId <= 232) {
+      return "thunderstorm";
+    }
+    return "";
+  };
+
   const searchLocation = (event) => {
     if (event.key === "Enter") {
-      axios.get(api_url)
-        .then((response) => {
-          setData(response.data);
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.error("Erreur lors de la récupération des données météo :", error.response ? error.response.data : error.message);
-        });
-      setLocation('');
+      axios.get(api_url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
     }
-  }  
+  };
 
   return (
-    <div className="app">
+    <div className={`app ${getWeatherClass()}`}>
       <div className="search">
         <input
           value={location}
@@ -46,22 +70,22 @@ function App() {
           </div>
         </div>
 
-        {data.name !== undefined &&
-        <div className="bottom">
-          <div className="feels">
-            {data.main ? <p className="bold">{Math.round(data.main.feels_like)}°C</p> : null}
-            <p>Ressentie</p>
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className="bold">{Math.round(data.main.feels_like)}°C</p> : null}
+              <p>Ressentie</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Humidité</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className="bold">{Math.round(data.wind.speed)} KM/H</p> : null}
+              <p>Vent</p>
+            </div>
           </div>
-          <div className="humidity">
-            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-            <p>Humidité</p>
-          </div>
-          <div className="wind">
-            {data.wind ? <p className="bold">{Math.round(data.wind.speed)}KM/H</p> : null}
-            <p>Vent</p>
-          </div>
-        </div>
-        }
+        )}
       </div>
     </div>
   );
